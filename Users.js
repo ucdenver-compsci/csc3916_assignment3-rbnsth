2f3e4ee1a19ed0d95c793629086e90db996cd145
Robin Shrestha <robinshrestha089@gmail.com>
2024-03-10 12:56:22 -0600

    Remove commented out code for MongoDB connection

--- Users.js
+++ Users.js
@@ -11,13 +11,6 @@ try {
     console.log("could not connect");
 }
 
-// try {
-//     mongoose.connect('mongodb+srv://rbnsth:PeZzwTjUYTQYPmev@cluster0.mongodb.net/webapi?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }, () =>
-//         console.log("connected"));
-// } catch (error) {
-//     console.log("could not connect");
-// }
-
 mongoose.set('useCreateIndex', true);
 
 //user schema
